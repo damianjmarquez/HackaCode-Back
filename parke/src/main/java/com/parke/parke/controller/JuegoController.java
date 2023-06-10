7d61fae0b0de2dd351cef81fe5ae51cb8dfc2bbf
damianjmarquez <69488200+damianjmarquez@users.noreply.github.com>
2023-06-10 06:33:56 -0300

    crud y prueba usuario

---
+++ parke/src/main/java/com/parke/parke/controller/JuegoController.java
@@ -0,0 +1,67 @@
+package com.parke.parke.controller;
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.parke.parke.model.Juego;
+
+import com.parke.parke.service.IJuegoService;
+
+
+@RestController
+public class JuegoController {
+    
+    @Autowired
+    private IJuegoService juegoServ;
+    
+    @GetMapping ("/juegos/traer")
+    public List<Juego> getJuego() {
+        
+        return juegoServ.getJuego();
+    }
+    
+    @PostMapping ("/juegos/crear")
+    public String saveJuego (@RequestBody Juego juego) {
+    	juegoServ.saveJuego(juego);
+        
+        return "el juego fue creada correctamente";
+    }
+    
+    @DeleteMapping ("/juegos/borrar/{id}")
+    public String deleteJuego(@PathVariable int id) {
+    	juegoServ.deleteJuego(id);
+        return "los juegos fue eliminada correctamente";
+    }
+    
+    @PutMapping ("/juegos/editar/{id_original}")
+    public Juego editJuego (@PathVariable int id_original,
+            @RequestParam(required = false, name = "id") int nuevaId,
+            @RequestParam(required = false, name = "nombre") String nuevoNombre,
+            @RequestParam(required = false, name = "personas") int nuevaCantidad,
+            @RequestParam(required = false, name = "precio") int nuevoPrecio) {
+            
+    	juegoServ.editJuego(id_original, nuevaId, nuevoNombre, nuevaCantidad, nuevoPrecio);
+        
+        Juego juego = juegoServ.findJuego(nuevaId);
+        
+        return juego;
+
+    }
+    
+    @PutMapping ("/juegos/editar")
+    public Juego editJuego(@RequestBody Juego juego) {
+    	juegoServ.editJuego(juego);
+    	
+    	return juegoServ.findJuego(juego.getId());
+    }
+
+}
---
+++ parke/src/main/java/com/parke/parke/service/JuegoService.java
@@ -0,0 +1,77 @@
+package com.parke.parke.service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.parke.parke.model.Juego;
+
+import com.parke.parke.repository.IJuegoRepository;
+
+
+
+@Service
+public class JuegoService implements IJuegoService {
+	
+	@Autowired
+    private IJuegoRepository juegoRepo;
+
+	@Override
+	public List<Juego> getJuego() {
+		List<Juego> listaCompradores = juegoRepo.findAll();
+        return listaCompradores;
+	}
+
+	@Override
+	public void saveJuego(Juego juego) {
+		juegoRepo.save(juego);
+		
+	}
+
+	@Override
+	public void deleteJuego(int id) {
+		juegoRepo.deleteById(id);
+		
+	}
+
+	@Override
+	public Juego findJuego(int id) {
+		Juego juego = juegoRepo.findById(id).orElse(null);
+        return juego;
+	}
+
+	
+
+	
+
+	@Override
+	public void editJuego(int idOriginal, int idNueva, String nuevoNombre, int nuevaCantidad,
+			int nuevoPrecio) {
+
+		//busco  el objeto original
+    	Juego juego = this.findJuego(idOriginal);
+            
+            //proceso de modificación a nivel lógico
+    	juego.setId(idNueva);
+    	juego.setNombre(nuevoNombre);
+    	juego.setCantidadDePersonas(nuevaCantidad);
+    	juego.setPrecio(nuevoPrecio);
+    	
+            
+            
+            //guardar los cambios
+            this.saveJuego(juego);
+		
+	}
+	
+	
+
+	@Override
+	public void editJuego (Juego juego) {
+		// TODO Auto-generated method stub
+		
+	}
+}
